12550ff763bcb414d2cd19eb0af17bbd38629f4f
kamil@huggingface.co <kamilakesbi@gmail.com>
2024-04-02 09:11:15 +0000

    up

--- train_segmentation.py
+++ train_segmentation.py
@@ -16,11 +16,6 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     # dataset arguments:
-    # parser.add_argument(
-    #     "--load_processed_dataset",
-    #     help="",
-    #     default="kamilakesbi/real_ami_ihm_processed",
-    # )
     parser.add_argument("--dataset_name", help="", default="kamilakesbi/real_ami_ihm")
     # Preprocess arguments:
 
@@ -28,7 +23,7 @@ if __name__ == "__main__":
     parser.add_argument("--from_pretrained", help="", default=True)
 
     # Training Arguments:
-    parser.add_argument("--learning_rate", help="", default=1e-3)
+    parser.add_argument("--lr", help="", default=1e-3)
     parser.add_argument("--batch_size", help="", default=32)
     parser.add_argument("--epochs", help="", default=3)
 
@@ -36,13 +31,18 @@ if __name__ == "__main__":
     parser.add_argument("--do_init_eval", help="", default=True)
     parser.add_argument('--checkpoint_path', help="", default='checkpoints/ami')
     parser.add_argument('--save_model', help="", default=True)
+
+    # Hardware args: 
+    parser.add_argument('--num_proc', help="", default=12)
+
     args = parser.parse_args()
 
-    dataset = load_dataset(str(args.dataset_name), num_proc=12)
+    dataset = load_dataset(str(args.dataset_name), num_proc=int(args.num_proc))
     
     model = SegmentationModel()
 
-    if str(args.from_pretrained):
+    if args.from_pretrained is True:
+        print('ok')
         pretrained = Model.from_pretrained(
             "pyannote/segmentation-3.0", use_auth_token=True
         )
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     
     preprocessed_dataset = Preprocess(
         dataset, model
-    ).preprocess_dataset(num_proc=24)
+    ).preprocess_dataset(num_proc=int(args.num_proc))
     
     train_dataset = preprocessed_dataset["train"].with_format("torch")
     eval_dataset = preprocessed_dataset["validation"].with_format("torch")
@@ -61,12 +61,12 @@ if __name__ == "__main__":
         output_dir=str(args.checkpoint_path),
         evaluation_strategy="epoch",
         save_strategy="epoch",
-        learning_rate=1e-3,
-        per_device_train_batch_size=32,
+        learning_rate=float(args.lr),
+        per_device_train_batch_size=int(args.batch_size),
         gradient_accumulation_steps=1,
         per_device_eval_batch_size=32,
-        dataloader_num_workers=8,
-        num_train_epochs=3,
+        dataloader_num_workers=int(args.num_proc),
+        num_train_epochs=int(args.epochs),
         logging_steps=200,
         load_best_model_at_end=True,
         push_to_hub=False,
@@ -83,16 +83,11 @@ if __name__ == "__main__":
         compute_metrics=metrics.der_metric,
     )
 
-    if str(args.do_init_eval):
+    if args.do_init_eval is True:
         first_eval = trainer.evaluate()
         print("Initial metric values: ", first_eval)
     trainer.train()
 
-    if str(args.save_model): 
+    if args.save_model is True: 
         trainer.save_model(output_dir=str(args.checkpoint_path))
     
-
-
-
-
-
