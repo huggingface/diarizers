a5b58d09d328756eb03a9352f852ac211086271b
kamil@huggingface.co <kamilakesbi@gmail.com>
2024-04-15 15:13:00 +0000

    up

--- src/diarizers/__init__.py
+++ src/diarizers/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "0.0.0"
+__version__ = "0.1"
+
+from .data import Preprocess, SpeakerDiarizationDataset
+from .models.segmentation import SegmentationModel, SegmentationModelConfig
+from .utils import Metrics, DataCollator, train_val_test_split
+from .test import Test
